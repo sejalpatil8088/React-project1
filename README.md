cd3b7f47766152ff66dd99b4787f7f7b4a031e65
sejalpatil8088 <patilsejal8088@gmail.com>
2023-02-28 20:01:44 +0530

    changes added

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## Here I created simple Todo App using React Js . In this App I add features like create new todo, View all todo, edit existing todo, and Delete existing Todo.
+
+## Todo App Using React Js
+### Here I created simple Todo App using React Js . In this App I add features like create new todo, View all todo, edit existing todo, and Delete existing Todo.
---
+++ src/components/TodoForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoForm = ({handleSubmit,todo,editId,setTodo}) => {
+    return(
+        <form className="todoForm" onSubmit={handleSubmit}>
+            <input
+               type="text"
+               placeholder="Enter Your Todo"
+               value={todo}
+               onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit">{editId ? "Edit" : "+"}</button>
+        </form>
+    );
+};
+
+export default TodoForm;
